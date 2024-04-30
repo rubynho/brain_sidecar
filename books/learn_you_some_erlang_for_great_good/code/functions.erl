bcd16b43865619a821859f01f3a39a0ba92735f7
John Doe <brunorafael6656@gmail.com>
2024-04-30 00:08:20 -0300

    Add function guards example

--- books/learn_you_some_erlang_for_great_good/code/functions.erl
+++ books/learn_you_some_erlang_for_great_good/code/functions.erl
@@ -1,5 +1,5 @@
 -module(functions).
--export([head/1, second/1]).
+-export([head/1, second/1, same/2, right_age/1, wrong_age/1]).
 
 head([X|_]) -> X.
 
@@ -7,3 +7,13 @@ second([_,X|_]) -> X.
 
 same(X,X) -> true;
 same(_,_) -> false.
+
+% The functions below are equivalent.
+% Using `,` as a guard separator acts as a `andalso` all guards must pass.
+% Using `;` acts as a `oralso`, if the first guard pass the others are not checked, otherwise the next is checked.
+
+right_age(X) when X >= 16, X =< 104 -> true;
+right_age(_) -> false.
+
+wrong_age(X) when X < 16, X > 104 -> true;
+wrong_age(_) -> false.
