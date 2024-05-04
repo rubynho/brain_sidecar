00c7972881758c34dde373de5dbe730527fb6123
John Doe <brunorafael6656@gmail.com>
2024-05-03 23:36:00 -0300

    Add recursive function to find the length of lists

--- books/learn_you_some_erlang_for_great_good/code/recursive.erl
+++ books/learn_you_some_erlang_for_great_good/code/recursive.erl
@@ -3,3 +3,19 @@
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N * fac(N - 1).
+
+% Recursively finds the lenght of a list.
+% It would unfold like so:
+% len([1, 2, 3, 4]) = len([1 | [2, 3, 4]])
+%                   = 1 + len([2 | [3, 4]])
+%                   = 1 + 1 + len([3 | [4]])
+%                   = 1 + 1 + 1 + len([4 | []])
+%                   = 1 + 1 + 1 + 1 + len([])
+%                   = 1 + 1 + 1 + 1 + 0
+%                   = 1 + 1 + 1 + 1
+%                   = 1 + 1 + 2
+%                   = 1 + 3
+%                   = 4
+
+len([]) -> 0;
+len([_, T]) -> 1 + len(T).
