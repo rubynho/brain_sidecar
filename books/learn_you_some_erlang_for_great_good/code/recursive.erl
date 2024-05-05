55e1d2cc711c102f5cb6c6dc638c806ec4f0f520
John Doe <brunorafael6656@gmail.com>
2024-05-05 00:10:48 -0300

    Add iterative version of the factorial function

--- books/learn_you_some_erlang_for_great_good/code/recursive.erl
+++ books/learn_you_some_erlang_for_great_good/code/recursive.erl
@@ -4,6 +4,12 @@
 fac(0) -> 1;
 fac(N) when N > 0 -> N * fac(N - 1).
 
+% The function above has a linear growth, which means it grows as much as there elements. To write an iterative version, an extra param is necessary to accumulate de result. It's called tail recursion.
+tail_fac(N) -> tail_fac(N, 1).
+tail_fac(0, Acc) -> Acc;
+tail_fac(N, Acc) -> when N > 0 -> tail_fac(N - 1, N * Acc);
+
+
 % Recursively finds the lenght of a list.
 % It would unfold like so:
 % len([1, 2, 3, 4]) = len([1 | [2, 3, 4]])
