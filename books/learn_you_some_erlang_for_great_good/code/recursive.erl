4ed0ea38360d79f471730bfb52f3310b3ebc9fe4
John Doe <brunorafael6656@gmail.com>
2024-05-05 23:44:51 -0300

    Add quicksort algorithm written in erlang

--- books/learn_you_some_erlang_for_great_good/code/recursive.erl
+++ books/learn_you_some_erlang_for_great_good/code/recursive.erl
@@ -25,3 +25,19 @@ tail_fac(N, Acc) -> when N > 0 -> tail_fac(N - 1, N * Acc);
 
 len([]) -> 0;
 len([_, T]) -> 1 + len(T).
+
+% Quicksort algorithm
+quicksort([]) -> [];
+quicksort([Pivot | Rest]) ->
+    {Smaller, Larger} = partition(Pivot, Rest, [], []),
+    quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+partition(_, [], Smaller, Larger) -> {Smaller, Larger};
+partition(Pivot, [H, T], Smaller, Larger) ->
+    if H =< Pivot ->
+        partition(Pivot, T, [H, Smaller], Larger);
+    H > Pivot ->
+        partition(Pivot, T, Smaller, [H, Larger])
+    end.
+
+
